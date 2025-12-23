0d54fb4ca9ad08de8a3b64bdd4c48e426db64b45
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-12-23 19:42:04 +0100

    f

--- src/mods/close/mod.tsx
+++ src/mods/close/mod.tsx
@@ -1,5 +1,6 @@
-import { Nullable, Option } from "@hazae41/option"
-import { createContext, useContext } from "react"
+import { Nullable } from "@/libs/nullable/mod.ts";
+import { Option } from "@hazae41/result-and-option";
+import { createContext, useContext } from "react";
 
 export type Close = (force?: boolean) => void
 
